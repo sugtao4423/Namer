08c46bd45cf2ddba58621ed4fa6f7d2dd44ba8ad
tao <sugtao4423@gmail.com>
2015-03-14 01:27:48 +0900

    たおっぱいのNamer
    正規表現で判定するようにしたら固まるようになった気がしたので元に戻した

--- src/Streaming.java
+++ src/Streaming.java
@@ -7,9 +7,6 @@ import java.io.OutputStreamWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import twitter4j.Status;
 import twitter4j.StatusUpdate;
 import twitter4j.Twitter;
@@ -19,7 +16,7 @@ import twitter4j.UserStreamAdapter;
 
 
 class Streaming extends UserStreamAdapter{
-	public String message;
+	public String tweet, MyScreenName, message;
 	public long MyUserId;
 	Twitter twitter = Namer.twitter;
 	Namer namer = new Namer();
@@ -31,72 +28,62 @@ class Streaming extends UserStreamAdapter{
 		String user = status.getUser().getScreenName();
 		long tweetId = status.getId();
 		Date CreatedAt = status.getCreatedAt();
-		//System.out.println(tweet);
-		Pattern pattern = Pattern.compile("^@" + Namer.MyScreenName + "+\\s(.+)+\\s(.+)|^@" + Namer.MyScreenName + "+\\s(.+)");
-		Matcher matcher = pattern.matcher(tweet);
-		if(matcher.find()){
-			String command = null;
-			String text = null;
-			if(matcher.group(3) == null)
-				command = matcher.group(1);
-			else
-				command = matcher.group(3);
-			text = matcher.group(2);
-			
-			try{
-			//MyScreenName = "@" + twitter.getScreenName();
+		try{
+			MyScreenName = "@" + twitter.getScreenName();
 			MyUserId = twitter.getId();
 			//名前変更
-			if(command.equals("名前変更")){
+			if(tweet.startsWith(MyScreenName + " 名前変更 ")){
 				show(tweet, user, CreatedAt);
-				if(text.length() > 20)
+				if(tweet.substring(MyScreenName.length() + 6).length() > 20)
 					namer.ChageNameError(user, tweetId);
 				else
-					namer.updateName(text, user, tweetId);
+					namer.updateName(tweet.substring(MyScreenName.length() + 6), user, tweetId);
 			}
 			//bio変更
-			if(command.equals("bio変更") || command.equals("BIO変更")){
+			if(tweet.startsWith(MyScreenName + " bio変更 ") || tweet.startsWith(MyScreenName + " BIO変更 ")){
 				show(tweet, user, CreatedAt);
-				if(text.length() > 160)	
+				if(tweet.substring(MyScreenName.length() + 7).length() > 160)	
 					namer.ChangeBioError(user, tweetId);
 				else
-					namer.updateBio(text, user, tweetId);
+					namer.updateBio(tweet.substring(MyScreenName.length() + 7), user, tweetId);
 			}
 			//新しいツイート
-			if(command.equals("新しいツイート")){
+			if(tweet.startsWith(MyScreenName + " 新しいツイート ")){
 				show(tweet, user, CreatedAt);
-				if(text.length() > 140)
+				if(tweet.substring(MyScreenName.length() + 9).length() > 140)
 					namer.LongTweetStringError(user, tweetId);
 				else
-					namer.newTweet(text, user, tweetId);
+					namer.newTweet(tweet.substring(MyScreenName.length() + 9), user, tweetId);
 			}
 			//好き？
-			if(command.equals("好き？")){
-				show(tweet, user, CreatedAt);
-				Random rnd = new Random();
-				int ran = rnd.nextInt(99);
-				if(ran == 1)
-					namer.Like(user, tweetId);
-				else
-					namer.DoNotLike(user, tweetId);
+			if(tweet.startsWith(MyScreenName + " 好き？")){
+				if(tweet.length() < MyScreenName.length() + 10){
+					show(tweet, user, CreatedAt);
+					Random rnd = new Random();
+					int ran = rnd.nextInt(99);
+					if(ran == 1)
+						namer.Like(user, tweetId);
+					else
+						namer.DoNotLike(user, tweetId);
+				}
 			}
 			//status
-			if(command.equals("status")){
+			if(tweet.startsWith(MyScreenName + " status")){
 				show(tweet, user, CreatedAt);
 				namer.WorkingNamer(user, tweetId);
 			}
 			//NamerMemory
-			if(command.equals("NamerMemory")){
+			if(tweet.matches(MyScreenName + " NamerMemory")){
 				show(tweet, user, CreatedAt);
 				namer.NamerMemoryTweet(user, tweetId);
 			}
 			//ping
-			if(command.equals("ping")){
+			if(tweet.startsWith(MyScreenName + " ping")){
 				show(tweet, user, CreatedAt);
 				namer.ping(user, tweetId);
 			}
 			//NamerStop
-			if(command.equals("NamerStop")){
+			if(tweet.matches(MyScreenName + " NamerStop")){
 				show(tweet, user, CreatedAt);
 				namer.NamerStop();
 			}
@@ -106,48 +93,47 @@ class Streaming extends UserStreamAdapter{
 				}catch (Exception e1) {
 				}
 			}
-			//Namer-setDefault
-			if(command.equals("Namer-setDefault") && status.getUser().getId() == MyUserId && !status.getSource().replaceAll("<.+?>", "").equals("たおっぱいのNamer")){
-				show(tweet, user, CreatedAt);
-				try {
-					User me = namer.twitter.verifyCredentials();
-					String profile[] = new String[4];
-					profile[0] = me.getName();
-					profile[1] = me.getDescription();
-					profile[2] = me.getLocation();
-					profile[3] = me.getURL();
-					FileOutputStream fos = new FileOutputStream("/var/www/html/NamerLog/Default/" + Namer.MyScreenName + ".txt");
-					OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");
-					BufferedWriter bw = new BufferedWriter(osw);
-					for(int i = 0; i < 4; i++)
-						bw.write(profile[i] + "\n");
-					bw.flush();
-					bw.close();
-					fos.close();
-					message = "@" + Namer.MyScreenName + " デフォルトのプロフィールを保存しました " + namer.date();
-					namer.twitter.updateStatus(new StatusUpdate(message).inReplyToStatusId(tweetId));
-					namer.show(message, true);
-				} catch (Exception e) {
-				}
-			}
-			//Namer-loadDefault
-			if(command.equals("Namer-loadDefault") && status.getUser().getId() == MyUserId && !status.getSource().replaceAll("<.+?>", "").equals("たおっぱいのNamer")){
-				show(tweet, user, CreatedAt);
-				try{
-				FileInputStream fis = new FileInputStream("/var/www/html/NamerLog/Default/" + Namer.MyScreenName + ".txt");
-				BufferedReader br = new BufferedReader(new InputStreamReader(fis,"UTF-8"));
+		//Namer-setDefault
+		if(tweet.startsWith(MyScreenName + " Namer-setDefault") && status.getUser().getId() == MyUserId && tweet.length() == MyScreenName.length() + 17){
+			show(tweet, user, CreatedAt);
+			try {
+				User me = namer.twitter.verifyCredentials();
 				String profile[] = new String[4];
-				//0：名前 1：bio 2：場所 3：URL
+				profile[0] = me.getName();
+				profile[1] = me.getDescription();
+				profile[2] = me.getLocation();
+				profile[3] = me.getURL();
+				FileOutputStream fos = new FileOutputStream("/var/www/html/NamerLog/Default/" + MyScreenName.substring(1) + ".txt");
+				OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");
+				BufferedWriter bw = new BufferedWriter(osw);
 				for(int i = 0; i < 4; i++)
-					profile[i] = br.readLine();
-				fis.close();
-				br.close();
-				message = "@" + Namer.MyScreenName + " プロフィールをデフォルトに戻しました " + namer.date();
-				namer.twitter.updateProfile(profile[0], profile[3], profile[2], profile[1]);
+					bw.write(profile[i] + "\n");
+				bw.flush();
+				bw.close();
+				fos.close();
+				message = MyScreenName + " デフォルトのプロフィールを保存しました " + namer.date();
 				namer.twitter.updateStatus(new StatusUpdate(message).inReplyToStatusId(tweetId));
 				namer.show(message, true);
-				}catch(Exception e){
-				}
+			} catch (Exception e) {
+			}
+		}
+		//Namer-loadDefault
+		if(tweet.startsWith(MyScreenName + " Namer-loadDefault") && status.getUser().getId() == MyUserId && tweet.length() == MyScreenName.length() + 18){
+			show(tweet, user, CreatedAt);
+			try{
+			FileInputStream fis = new FileInputStream("/var/www/html/NamerLog/Default/" + MyScreenName.substring(1) + ".txt");
+			BufferedReader br = new BufferedReader(new InputStreamReader(fis,"UTF-8"));
+			String profile[] = new String[4];
+			//0：名前 1：bio 2：場所 3：URL
+			for(int i = 0; i < 4; i++)
+				profile[i] = br.readLine();
+			fis.close();
+			br.close();
+			message = MyScreenName + " プロフィールをデフォルトに戻しました " + namer.date();
+			namer.twitter.updateProfile(profile[0], profile[3], profile[2], profile[1]);
+			namer.twitter.updateStatus(new StatusUpdate(message).inReplyToStatusId(tweetId));
+			namer.show(message, true);
+			}catch(Exception e){
 			}
 		}
 	}
